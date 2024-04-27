bb3e2d5696540a3244c89b2abd9fafc1f5c12b65
Stanislav (Stas) Katkov <git@skatkov.com>
2024-04-27 23:21:07 +0200

    compile through github actions ci

--- compile_protoc_gen_twirp_ruby.sh
+++ compile_protoc_gen_twirp_ruby.sh
@@ -6,8 +6,7 @@ platforms=("darwin/amd64" "darwin/arm64" "linux/amd64" "linux/arm64")
 
 # Set the output directory to the current directory
 output_dir="./dist"
-version="1.11.0"
-
+version="${VERSION:-1.11.0}"
 
 # Create the output directory if it doesn't exist
 mkdir -p "$output_dir"
@@ -27,9 +26,20 @@ for platform in "${platforms[@]}"; do
     export GOARCH="$arch"
 
     # Compile the plugin
-    go build -o "$output_dir/protoc-gen-twirp_ruby-$os-$arch" github.com/arthurnn/twirp-ruby/protoc-gen-twirp_ruby
+    go build -o "$output_dir/protoc-gen-twirp_ruby" github.com/arthurnn/twirp-ruby/protoc-gen-twirp_ruby
 
     echo "Compiled protoc-gen-twirp_ruby for $os/$arch and placed it in $output_dir"
+
+    # Archive the compiled executable
+    tar_name="protoc-gen-twirp_ruby-$version-$os-$arch.tar.gz"
+    tar -czvf "$output_dir/$tar_name" -C "$output_dir" "protoc-gen-twirp_ruby"
+
+    echo "Archived protoc-gen-twirp_ruby to $output_dir/$tar_name"
+
+    # Remove the executable after archiving
+    rm "$output_dir/protoc-gen-twirp_ruby"
+
+    echo "Removed the executable $output_dir/protoc-gen-twirp_ruby"
 done
 
 echo "All protoc-gen-twirp_ruby binaries have been compiled and placed in the current directory."
