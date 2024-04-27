d5db17210763e9d95fcaf7a986471ec00f512873
Stanislav (Stas) Katkov <git@skatkov.com>
2024-04-27 22:49:56 +0200

    compile under all linux/darwin platform, ditch windows

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Nothing to see here, just compiled executable of github.com/arthurnn/twirp-ruby/protoc-gen-twirp_ruby
+
+
+Creating a release in github:
+https://github.com/benbjohnson/litestream/blob/main/.github/workflows/release.linux.yml
--- compile_protoc_gen_twirp_ruby.sh
+++ compile_protoc_gen_twirp_ruby.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 # Set the target platforms
-platforms=("darwin/amd64" "linux/amd64" "windows/amd64")
+platforms=("darwin/amd64" "darwin/arm64" "linux/amd64" "linux/arm64")
+
 
 # Set the output directory to the current directory
-output_dir="."
+output_dir="./dist"
+version="1.11.0"
+
 
 # Create the output directory if it doesn't exist
 mkdir -p "$output_dir"
 
+
+go get github.com/arthurnn/twirp-ruby/protoc-gen-twirp_ruby@v$version
 # Compile and install the plugin for each platform
 for platform in "${platforms[@]}"; do
     IFS='/' read -ra parts <<< "$platform"
